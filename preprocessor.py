1c8464a7d4cf8c57fbe308f5566038c1a0703311
huranech <hugo.ran142@gmail.com>
2023-11-14 11:15:20 +0100

    carcasa preprocessor.py

---
+++ preprocessor.py
@@ -0,0 +1,24 @@
+
+
+def preprocesar_texto(texto_crudo):
+    '''
+    '''
+    pass
+
+
+def tfidf(documentos):
+    '''
+    '''
+    pass
+
+
+def we(documentos):
+    '''
+    '''
+    pass
+
+
+def transformers(documentos):
+    '''
+    '''
+    pass
