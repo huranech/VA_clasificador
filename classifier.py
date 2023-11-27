04d55fdab2d9261e1c64ca7d00d30a3bcc21deab
huranech <hugo.ran142@gmail.com>
2023-11-27 19:18:35 +0100

    prepost de classifier

--- classifier.py
+++ classifier.py
@@ -3,8 +3,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 
-def entrenar_svm(x_matrix, y_labels, output_file):
+def entrenar_svm(x_matrix, y_labels):
     '''
+    Precondiciones:
+        x_matrix es un dataframe de pandas que contiene estructuras vectoriales que hacen referencia a las
+        autopsias verbales de cada instancia. 
+        y_labels es una lista con las etiquetas reales de las instancias.
+        el orden de las instancias en x_matrix es el mismo que en y_labels.
+    Poscondiciones:
+        imprime figuras de mérito para una tarea de clasificación. Concretamente la precisión, el recall y el fscore.
+        devuelve un modelo SVM entrenado para las instancias de x_matrix e y_labels.
     '''
     # separar train y test (70%-30%)
     x_train, x_test, y_train, y_test = train_test_split(x_matrix, y_labels, test_size=0.3, random_state=42)
@@ -27,6 +35,16 @@ def entrenar_svm(x_matrix, y_labels, output_file):
 
 
 def devolver_fscore_svm(x_matrix, y_labels):
+    '''
+    Precondiciones:
+        x_matrix es un dataframe de pandas que contiene estructuras vectoriales que hacen referencia a las
+        autopsias verbales de cada instancia. 
+        y_labels es una lista con las etiquetas reales de las instancias.
+        el orden de las instancias en x_matrix es el mismo que en y_labels.
+    Poscondiciones:
+        devuelve la precisión, el recall y el fscore para una tarea de clasificación con
+        el algoritmo SVM aplicado a x_matrix e y_labels
+    '''
     # separar train y test (70%-30%)
     x_train, x_test, y_train, y_test = train_test_split(x_matrix, y_labels, test_size=0.3, random_state=42)
 
